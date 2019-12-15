200c0e8d3137e06aadda64a0d2c4faab891b60c9
vesodeveloper <vesodeveloper@gmail.com>
2019-12-15 10:39:18 +0200

    upload files to new server

--- docs/Free/list.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dir="/public_html/free";
-$files=scandir($dir);
-foreach(scandir($dir) as $files)
-echo $files. "</br>";
- ?>
--- docs/Free/op.php
+++
@@ -1,7 +0,0 @@
-<?php
-$openfile=$_POST["fop"];
-if(file_exists(data.free)){
-$file=fopen("data.free","r") or die("Error!") ;
-echo fread($file,filesize("data.free"));}
-else echo "File isn't exists!";
- ?>
